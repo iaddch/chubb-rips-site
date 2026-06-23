1e0112c1a81e67f4807e1610a2a249339891fee5
iaddch <iaddche@gmail.com>
2026-06-23 17:03:14 -0400

    fixed base issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'chubb-rips-site',
+  base: '/chubb-rips-site/',
   plugins: [react()],
 })
